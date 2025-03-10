28c235ba8326a3c01006193781af478439a6863d
Olivfhan <joselegtd@gmail.com>
2025-03-10 19:20:41 +0100

    feat: refactored allergen to track foods containing them

--- src/main/java/com/isppG8/infantem/infantem/allergen/Allergen.java
+++ src/main/java/com/isppG8/infantem/infantem/allergen/Allergen.java
@@ -1,49 +1,44 @@
 package com.isppG8.infantem.infantem.allergen;
 
-import jakarta.persistence.*;
+
+import com.isppG8.infantem.infantem.intake.IngredientRecipe;
+
+import java.util.List;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import jakarta.persistence.CascadeType;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Allergen {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE	, generator = "entity_seq")
+    private Integer Id;
 
+    @NotBlank
+    @Size(min = 3, max = 100)
     private String name;
+
+    @NotBlank
+    @Size(min = 3, max = 200)
     private String description;
 
-   
-    public Allergen() {
-    }
+    //Relaciones
 
-    public Allergen(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
-
-    
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    @OneToMany(mappedBy = "id", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<IngredientRecipe> ingredientsWithAllergen;
 }
+
 
