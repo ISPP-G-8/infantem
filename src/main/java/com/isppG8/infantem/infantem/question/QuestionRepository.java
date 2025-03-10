83610aa8376bb48777a034bf30aa46a46f02f4a2
Olivfhan <joselegtd@gmail.com>
2025-03-10 19:21:09 +0100

    feat: created Question entity to track previously asked questions

---
+++ src/main/java/com/isppG8/infantem/infantem/question/QuestionRepository.java
@@ -0,0 +1,11 @@
+package com.isppG8.infantem.infantem.question;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+    
+    @Query("SELECT q FROM Question q WHERE q.baby.id = :babyId")
+    List<Question> findByBabyId(int babyId);
+}
