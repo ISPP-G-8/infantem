849511fcc702570a6d6f0f6e9b0549b1a355f5ce
dedece65 <danicastillo2003@gmail.com>
2025-04-01 17:42:43 +0200

    feat: add Baby type definition and export in types index

---
+++ frontend/types/Baby.ts
@@ -0,0 +1,10 @@
+export type Baby = {
+    id?: number;
+    name: string;
+    birthDate: string;
+    genre?: string;
+    weight?: number;
+    height?: number;
+    cephalicPerimeter?: number;
+    foodPreference?: string;
+}
