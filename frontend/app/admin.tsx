9ab5140c3323a9d2c995bc9785afc4dbb561ef26
dedece65 <danicastillo2003@gmail.com>
2025-04-01 17:43:05 +0200

    feat: create Admin component to manage users and recommended recipes

---
+++ frontend/app/admin.tsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import { Recipe, User, Baby } from "../types";
+import { useAuth } from "../context/AuthContext"
+
+/*
+    allergen
+    baby
+    disease?
+    recipe
+    user
+*/
+
+
+export default function Admin() {
+    const [users, setUsers] = useState<User[]>([]);
+    const [recommendedRecipes, setRecommendedRecipes] = useState<Recipe[]>([]);
+    // const [babies, setBabies] = useState<Baby[]>([]);
+    // const [diseases, setDiseases] = useState<Disease[]>([]);
+    // const [allergens, setAllergens] = useState<Allergen[]>([]);
+
+    const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+    const { token } = useAuth();
+
+    useEffect(() => {
+        obtainAllUsers();
+        obtainAllRecommendedRecipes();
+        // obtainAllBabies();
+        // obtainAllDiseases();
+        // obtainAllAllergens();
+    }, []);
+
+    const obtainAllUsers = async (): Promise<boolean> => {
+        try {
+            const response = await fetch(`${apiUrl}/api/v1/users/all`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+            });
+
+            if (response.ok) {
+                const usersData = await response.json();
+                setUsers(usersData);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (error) {
+            console.error('Error fetching recipes: ', error);
+            setUsers([]);
+            return false;
+        }
+    };
+
+    const obtainAllRecommendedRecipes = async (): Promise<boolean> => {
+        try {
+            const response = await fetch(`${apiUrl}/api/v1/recipes/recommended`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+            });
+
+            if (response.ok) {
+                const recipesData = await response.json();
+                setRecommendedRecipes(recipesData);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (error) {
+            console.error('Error fetching recipes: ', error);
+            setRecommendedRecipes([]);
+            return false;
+        }
+    };
+
+    return (
+        <></>
+    );
+}
