523deb74bfc9382d6074c9a3080e1cdc23020919
Javier Santos <javsanmar5@alum.us.es>
2025-04-02 17:28:32 +0200

    fix: Permit all requests on explorer/*

--- src/main/java/com/isppG8/infantem/infantem/config/SecurityConfig.java
+++ src/main/java/com/isppG8/infantem/infantem/config/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
                 .exceptionHandling(
                         (exepciontHandling) -> exepciontHandling.authenticationEntryPoint(unauthorizedHandler))
                 .authorizeHttpRequests((requests) -> requests.requestMatchers("api/v1/recipes/*").authenticated()
-                        .requestMatchers("api/v1/auth/*").permitAll().anyRequest().authenticated())
+                        .requestMatchers("api/v1/auth/*").permitAll().requestMatchers("explorer/*").permitAll()
+                        .anyRequest().authenticated())
                 .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class)
                 .formLogin(AbstractHttpConfigurer::disable).httpBasic(Customizer.withDefaults());
         return http.build();
