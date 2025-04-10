b372c2929f65ef683f8b9b7f6af379a2ee9f10dc
josemorgado <josemorgado@users.noreply.github.com>
2025-04-10 13:55:52 +0000

    fix: Apply formatter

--- src/test/java/com/isppG8/infantem/infantem/advertisement/AdvertisementControllerTest.java
+++ src/test/java/com/isppG8/infantem/infantem/advertisement/AdvertisementControllerTest.java
@@ -14,7 +14,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
-
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.test.context.support.WithMockUser;
