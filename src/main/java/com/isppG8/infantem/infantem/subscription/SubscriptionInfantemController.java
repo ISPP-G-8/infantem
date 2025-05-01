daa2441178653a9d01e1ecbff4f63db6f8841035
Miguel Galán Lerate <miggaller@alum.us.es>
2025-05-01 22:56:02 +0200

    fix: fix linter

--- src/main/java/com/isppG8/infantem/infantem/subscription/SubscriptionInfantemController.java
+++ src/main/java/com/isppG8/infantem/infantem/subscription/SubscriptionInfantemController.java
@@ -1,7 +1,6 @@
 package com.isppG8.infantem.infantem.subscription;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties.Jwt;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
